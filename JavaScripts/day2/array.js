faf9748475bf6b23735bc1e731dddc9b42849af4
emoncse2020 <emon.cse.2020@gmail.com>
2025-04-15 10:51:36 +0600

    loop explored

---
+++ JavaScripts/day2/array.js
@@ -0,0 +1,17 @@
+// const array = ["sabbir", 12, "er", 34];
+
+// for (let i = 0; i < array.length; i++) {
+//   const element = array[i];
+//   console.log(element);
+// }
+
+const numbers = [1, 2, 4, 5, 5, 6, 3, 6, 3];
+numbers[0] = 77;
+numbers.push(99, 66);
+
+// const popa = numbers.pop();
+// console.log(popa);
+
+// numbers.unshift(88);
+
+console.log(numbers);
---
+++ JavaScripts/day2/for-of.js
@@ -0,0 +1,9 @@
+const num = [1, 2, 3, 4, 5, 5, 6];
+
+for (const element of num) {
+  console.log(element);
+}
+
+num.forEach((element) => {
+  console.log(element);
+});
